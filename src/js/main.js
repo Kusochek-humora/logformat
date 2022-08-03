3157ea018a7c7687f7a0c5c74d3dd9ea60851da1
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2022-08-03 12:27:48 +0600

    widget + adap header

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let menu = document.querySelector("widget");
+let items = document.querySelectorAll(".widget-item");
+let button = document.querySelector(".widget-button");
+let active = false;
+const length = items.length;
+const arc = 2 * Math.PI * (1 / length);
+const radius = 40;
+button.addEventListener("click", () => {
+  active = !active;
+  if (active) {
+    for (let i = 0; i < length; i++) {
+      const angle = i * arc;
+      const x = radius * Math.cos(angle);
+      const y = radius * Math.sin(angle);
+      items[i].style.opacity = 1;
+      items[i].style.left = 50 + x + "%";
+      items[i].style.top = 50 + y + "%";
+      button.classList.add("active");
+    }
+  } else {
+    for (let i = 0; i < length; i++) {
+      button.classList.remove("active");
+      items[i].removeAttribute("style");
+    }
+  }
+});
